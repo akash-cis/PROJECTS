d1905a3dd4fd5e75a4ebbd565b777774b2012859
Akash Singh <a.singh@crestinfosystems.net>
2021-11-11 09:16:59 +0530

    deleted zip file

--- DJANGO/django_proj/payment/utils.py
+++ DJANGO/django_proj/payment/utils.py
@@ -28,9 +28,45 @@ class StripeAccount():
         )
         self.user.stripe_id = customer_account["id"]
         self.user.save()
+        return customer_account
 
-    def update():
-        pass
+    def update(self):
+        customer_account = stripe.Customer.modify(
+            self.user.stripe_id,
+            email=self.user.email,
+            address={
+                "city":self.user.profile.city,
+                "state":self.user.profile.state,
+                "line1":self.user.profile.line1,
+                "country":self.user.profile.country,
+                "postal_code":self.user.profile.potal_code,
+            },
+            name=self.user.get_full_name(),
+            phone=self.user.contact
+        )
+        return customer_account
 
-    def delete():
-        pass
+    def delete(self):
+        try:
+            customer_account = stripe.Customer.delete(self.user.stripe_id)
+            return customer_account['deleted']
+        except stripe.error.StripeError as e:
+            message = 'Something went wrong while deleting the customer.'
+            return message
+        except Exception as e:
+            return e
+
+
+class StripePayment():
+    '''
+    this class will handle all sorts of payment business logics for the customer.
+    '''
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.get("user")
+        self.token = kwargs.get("token")
+        self.save_card = kwargs.get("save_card")
+        self.card_id = kwargs.get("card_id")
+        self.description = kwargs.get("description")
+        self.currency = kwargs.get("currency")
+        self.set_default = kwargs.get("set_default")
